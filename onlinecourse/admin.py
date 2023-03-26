1aca7596024f93f688a6ad413b589d598e345a31
favdauria <dauriafhc@gmail.com>
2023-03-26 17:17:26 -0400

    models

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 5
+
 
 
 # Register your models here.
@@ -17,10 +25,20 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
+    list_display = ('name', 'pub_date')
+    search_fields = ['name', 'description']
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_display = ('name', 'pub_date')
+    search_fields = ['name', 'description']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_display = ('name', 'pub_date')
+    search_fields = ['name', 'description']
 
 # <HINT> Register Question and Choice models here
 
@@ -28,3 +46,6 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
+
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -132,3 +132,29 @@ class Enrollment(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
+
+
+
+
+class Question(models.Model):
+    course = models.ManyToManyField(Course)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    content = models.TextField()
+    grade = models.IntegerField()
+
+    def is_get_score(self, selected_ids):
+       all_answers = self.choice_set.filter(is_correct=True).count()
+       selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+       if all_answers == selected_correct:
+           return True
+       else:
+           return False
+
+class Choice(models.Model):
+    question = models.ManyToManyField(Question)
+    content = models.TextField()
+    correct = models.BooleanField()
+
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice)
